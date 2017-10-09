373adc23451889e604fd4415351fee170d5a7199
tan <laosomboon@gmail.com>
2017-10-09 11:33:10 -0400

    adjust menu layout

--- js/fetchdata.js
+++ js/fetchdata.js
@@ -1,39 +1,41 @@
 function createMenuElement(ficon, menuName, menuPrice, menuDesc, menuImg) {
-        if(!menuPrice){
-            return null;
-        }
-        var div = document.createElement('div');
-        div.classList.add('mix');
-        div.classList.add(ficon);
-        div.classList.add('col-sm-10');
-        div.classList.add('menu-restaurant');
-        div.setAttribute('data-cat', ficon);
+    if (!menuPrice) {
+        return null;
+    }
+    var div = document.createElement('div');
+    div.classList.add('mix');
+    div.classList.add(ficon);
+    div.classList.add('col-xs-12');
+    div.classList.add('col-sm-12');
+    div.classList.add('col-md-6');
+    div.classList.add('menu-restaurant');
+    div.setAttribute('data-cat', ficon);
 
 
-        var spanWrapper = document.createElement('span');
-        spanWrapper.classList.add('clearfix');
+    var spanWrapper = document.createElement('span');
+    spanWrapper.classList.add('clearfix');
 
-        var a1 = document.createElement('a');
-        a1.setAttribute("class", "menu-title");
-        a1.setAttribute("data-meal-img", menuImg);
-        a1.innerHTML = menuName;
+    var a1 = document.createElement('a');
+    a1.setAttribute("class", "menu-title");
+    a1.setAttribute("data-meal-img", menuImg);
+    a1.innerHTML = menuName;
 
-        var lineSpan = document.createElement('span');
-        lineSpan.setAttribute("style", "left:166px; right: 44px;");
-        lineSpan.setAttribute("class", "menu-line");
+    var lineSpan = document.createElement('span');
+    lineSpan.setAttribute("style", "left:166px; right: 44px;");
+    lineSpan.setAttribute("class", "menu-line");
 
-        var priceSpan = document.createElement('span');
-        priceSpan.setAttribute("class", "menu-price");
-        priceSpan.innerHTML = menuPrice;
+    var priceSpan = document.createElement('span');
+    priceSpan.setAttribute("class", "menu-price");
+    priceSpan.innerHTML = menuPrice;
 
 
-        var subtitleSpan = document.createElement("span");
-        subtitleSpan.setAttribute("class", "menu-subtitle");
-        subtitleSpan.innerHTML = menuDesc;
+    var subtitleSpan = document.createElement("span");
+    subtitleSpan.setAttribute("class", "menu-subtitle");
+    subtitleSpan.innerHTML = menuDesc;
 
-        spanWrapper.appendChild(a1);
-        spanWrapper.appendChild(lineSpan);
-        spanWrapper.appendChild(priceSpan);
+    spanWrapper.appendChild(a1);
+    spanWrapper.appendChild(lineSpan);
+    spanWrapper.appendChild(priceSpan);
 
     div.appendChild(spanWrapper);
     div.appendChild(subtitleSpan);
@@ -69,7 +71,7 @@ dbRefObject.on('value', function(snap) {
 
             var elem = createMenuElement(c.icon, menuItem.name, menuItem.thaismile, menuItem.description, menuItem.image);
             var menuContainer = document.getElementById('Container');
-            if(elem){
+            if (elem) {
                 menuContainer.appendChild(elem);
             }
         });
